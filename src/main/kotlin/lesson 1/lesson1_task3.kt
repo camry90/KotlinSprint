687f2e5c6802df63c19b312a5f5339154bd512fb
camry90 <kamgidayatov@gmail.com>
2025-12-05 00:40:19 +0300

    KS-1-3 print variables

---
+++ src/main/kotlin/lesson 1/lesson1_task3.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    var year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
+}
