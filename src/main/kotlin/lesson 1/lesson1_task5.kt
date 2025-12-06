33c914acc65916862f5f021b7ff3c92be42bbf7d
camry90 <kamgidayatov@gmail.com>
2025-12-06 20:09:33 +0300

    KS-1-5-add constants and learned abt magic numbers

--- src/main/kotlin/lesson 1/lesson1_task5.kt
+++ src/main/kotlin/lesson 1/lesson1_task5.kt
@@ -1,12 +1,15 @@
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = 3600
+
 fun main() {
 
-    val flySeconds: Int = 6480
+    val flySeconds = 6480
 
-    val hours = flySeconds / 3600
-    val remainAfterHours= flySeconds % 3600
+    val hours = flySeconds / SECONDS_IN_HOUR
+    val remainAfterHours= flySeconds % SECONDS_IN_HOUR
 
-    val minutes = remainAfterHours / 60
-    val seconds = remainAfterHours % 60
+    val minutes = remainAfterHours / SECONDS_IN_MINUTE
+    val seconds = remainAfterHours % SECONDS_IN_MINUTE
 
     println("0${hours}:${minutes}:${seconds}0")
 
