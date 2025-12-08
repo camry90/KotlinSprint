7fb1056ee285fa3daa309a36cf52b86ad0a39eff
camry90 <kamgidayatov@gmail.com>
2025-12-08 15:16:15 +0300

    KS-2-5-bank percentage

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,13 +2,14 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
-const val INTEREST_RATE = 0.167
+const val INTEREST_RATE = 16.7
 const val INTEREST_YEAR = 20
 
 fun main() {
     val deposit = 70000
+    val interestRateKf = INTEREST_RATE / 100
 
-    val totalAmount = deposit * (1 + INTEREST_RATE).pow(INTEREST_YEAR.toDouble())
+    val totalAmount = deposit * (1 + interestRateKf).pow(INTEREST_YEAR.toDouble())
 
     println("%.3f".format(totalAmount))
 
