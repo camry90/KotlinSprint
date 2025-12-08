11aa699e36a816129f7ca827bab55186499b47ec
camry90 <kamgidayatov@gmail.com>
2025-12-08 17:39:30 +0300

    KS-3-3- Print multiplication table of 6

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+    val multiplyTableSix = 6
+
+    println("""
+        $multiplyTableSix * 1 = ${multiplyTableSix * 1}
+        $multiplyTableSix * 2 = ${multiplyTableSix * 2}
+        $multiplyTableSix * 3 = ${multiplyTableSix * 3}
+        $multiplyTableSix * 4 = ${multiplyTableSix * 4}
+        $multiplyTableSix * 5 = ${multiplyTableSix * 5}
+        $multiplyTableSix * 6 = ${multiplyTableSix * 6}
+        $multiplyTableSix * 7 = ${multiplyTableSix * 7}
+        $multiplyTableSix * 8 = ${multiplyTableSix * 8}
+        $multiplyTableSix * 9 = ${multiplyTableSix * 9}
+    """.trimIndent())
+}
