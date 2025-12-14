91d9abf9f097f31f61bac49fd4bdd13276693248
camry90 <kamgidayatov@gmail.com>
2025-12-14 16:11:29 +0300

    KS-4-1- Find available tables with operators

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,2 +1,17 @@
 package org.example.lesson_4
 
+const val TABLES_IN_RESTAURANT = 13
+
+fun main() {
+
+    val reservedTablesToday = 13
+    val reservedTablesTommorow = 9
+
+    val availableTablesToday = reservedTablesToday < TABLES_IN_RESTAURANT
+
+    val availableTablesTommorow = reservedTablesTommorow < TABLES_IN_RESTAURANT
+
+    println("Доступность столиков на сегодня: $availableTablesToday \n" +
+            "Доступность столиков на завтра: $availableTablesTommorow ")
+
+}
