b0855c7edfff804e2e637bf8f1b7e0df02799174
kamgi <kamgidayatov@gmail.com>
2025-12-17 14:37:37 +0300

    KS-4-5- Can boats sail criteria

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,2 +1,21 @@
 package org.example.lesson_4
 
+
+fun main() {
+
+    println("Наличие повреждение корпуса: ")
+    val hasDamage = readln().toBoolean()
+    println("Текущий состав экипажа: ")
+    val countPersonal = readln().toInt()
+    println("Количество ящиков с провизией на борту: ")
+    val countMealsBox = readln().toInt()
+    println("Благоприятность метеусловий: ")
+    val isWeatherGood = readln().toBoolean()
+
+    val canSail = (!hasDamage && countPersonal in 55..70 &&
+            countMealsBox > 50) || (hasDamage && countPersonal == 70 &&
+            isWeatherGood &&  countMealsBox >= 50)
+
+    println("Может ли корабль отправиться в долгосрочное плавание? $canSail")
+
+}
