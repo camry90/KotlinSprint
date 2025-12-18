bd7094c7a3d48f133b574f15fb70a1bc7aca82c4
camry90 <kamgidayatov@gmail.com>
2025-12-18 23:31:04 +0300

    KS-4-4-training day , delete valuable

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -4,13 +4,14 @@ fun main() {
 
     val trainingDay = 5
     val isArmsAndAbsDay = trainingDay % 2 == 0
-    val isLegsAndBackDay = !isArmsAndAbsDay
 
-    println("""
+    println(
+        """
         ${"Упражнение для рук:".padEnd(22)}$isArmsAndAbsDay
-        ${"Упражнение для ног:".padEnd(22)}$isLegsAndBackDay
-        ${"Упражнение для спины:".padEnd(22)}$isLegsAndBackDay
+        ${"Упражнение для ног:".padEnd(22)}${!isArmsAndAbsDay}
+        ${"Упражнение для спины:".padEnd(22)}${!isArmsAndAbsDay}
         ${"Упражнение для пресса:".padEnd(22)}$isArmsAndAbsDay
-    """.trimIndent())
+    """.trimIndent()
+    )
 
 }
