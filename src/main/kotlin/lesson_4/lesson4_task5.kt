01f68379de5eea48b74a3d2f7672053f9f526be9
camry90 <kamgidayatov@gmail.com>
2025-12-18 23:42:13 +0300

    KS-4-5-add constants

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,5 +1,8 @@
 package org.example.lesson_4
 
+const val MIN_PERSONAL = 55
+const val RECOMMENDED_PERSONAL = 70
+const val MEALS_BOX = 50
 
 fun main() {
 
@@ -12,9 +15,9 @@ fun main() {
     println("Благоприятность метеусловий: ")
     val isWeatherGood = readln().toBoolean()
 
-    val canSail = (!hasDamage && countPersonal in 55..70 &&
-            countMealsBox > 50) || (hasDamage && countPersonal == 70 &&
-            isWeatherGood &&  countMealsBox >= 50)
+    val canSail = (!hasDamage && countPersonal in MIN_PERSONAL..RECOMMENDED_PERSONAL &&
+            countMealsBox > MEALS_BOX) || (hasDamage && countPersonal == RECOMMENDED_PERSONAL &&
+            isWeatherGood &&  countMealsBox >= MEALS_BOX)
 
     println("Может ли корабль отправиться в долгосрочное плавание? $canSail")
 
