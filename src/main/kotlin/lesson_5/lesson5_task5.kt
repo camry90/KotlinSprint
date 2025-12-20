3a91bf9d0b04fee3cd340a574dc675ee9ba46202
kamgi <kamgidayatov@gmail.com>
2025-12-20 22:16:11 +0300

    KS-5-5- using lamda func

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -3,31 +3,26 @@ package org.example.lesson_5
 const val GUESSED_THREE_NUMS = 3
 const val GUESSED_TWO_NUMS = 2
 const val GUESSED_ONE_NUM = 1
+const val PICKS_COUNT = 3
 
 fun main() {
 
-    fun randomNum() = (1..42).random()
+    fun randomNum() = (0..42).random()
 
     val winNumsList = listOf(randomNum(), randomNum(), randomNum())
 
-    println("Введите первое число от 0 до 42: ")
-    val answerNum1 = readln().toInt()
-    println("Введите второе число от 0 до 42: ")
-    val answerNum2 = readln().toInt()
-    println("Введите третье число от 0 до 42: ")
-    val answerNum3 = readln().toInt()
+    val answerNumsList = List(PICKS_COUNT) { i ->
+        println("Введите число № ${i + 1} от 0 до 42: ")
+        readln().toInt()
+    }
 
-    val answerNumsList = listOf(answerNum1, answerNum2, answerNum3)
-    val res = winNumsList.intersect(answerNumsList)
+    val matchesCount = winNumsList.intersect(answerNumsList)
 
-    if (res.size == GUESSED_THREE_NUMS) {
-        println("Вы угадали все числа и выиграли джекпот!")
-    } else if (res.size == GUESSED_TWO_NUMS) {
-        println("Вы угадали два числа и выиграли крупный приз!")
-    } else if (res.size == GUESSED_ONE_NUM) {
-        println("Вы угадали одно число и получаете утешительный приз!")
-    } else {
-        println("Вы неугадали ни одно число и вы получаете ничего!")
+    when (matchesCount.size) {
+        GUESSED_THREE_NUMS -> println("Вы угадали все числа и выиграли джекпот!")
+        GUESSED_TWO_NUMS -> println("Вы угадалии два числа и выиграли крупный приз!")
+        GUESSED_ONE_NUM -> println("Вы угадали одно число и выиграли утешительный приз!")
+        else -> println("Вы не угадали ни одного числа")
     }
 
     println("Загаданные чилса: $winNumsList")
