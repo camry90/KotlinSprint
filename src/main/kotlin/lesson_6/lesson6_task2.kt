f80530fec6350319b2f71c66f9aa5a7c5bb5831a
kamgi <kamgidayatov@gmail.com>
2025-12-22 11:19:36 +0300

    KS-6-2- Simple timer with constants

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_6
 
+const val TIMER_END = 0
+
 fun main() {
 
     println("На сколько секунд засечь?")
     var seconds = readln().toInt()
 
-    while (seconds >= 0) {
+    while (seconds >= TIMER_END) {
         println("Прошло ${seconds--} ")
         Thread.sleep(1000)
     }
