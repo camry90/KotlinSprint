52d6f0f6dd6f9b8cf06292806385a9588520e6e1
kamgi <kamgidayatov@gmail.com>
2025-12-25 21:29:34 +0300

    KS-8-5- User ingredients list

--- src/main/kotlin/lesson_8/lesson8_task5.kt
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -6,10 +6,9 @@ fun main() {
     val countIngredients = readln().toInt()
     val ingredientsArray = Array(countIngredients) { "" }
 
-
-    for (i in 1..countIngredients) {
-        println("Введите $i инредиент: ")
-        ingredientsArray[i - 1] = readln()
+    for (i in ingredientsArray.indices) {
+        println("Введите ${i+1} инредиент: ")
+        ingredientsArray[i] = readln()
     }
 
     println(ingredientsArray.joinToString(", "))
