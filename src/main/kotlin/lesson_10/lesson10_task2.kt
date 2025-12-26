0d2ab1bc60dce522104fad4e23095b865bd65aa9
kamgi <kamgidayatov@gmail.com>
2025-12-26 16:53:47 +0300

    KS-10-2- length valid fun for signUp

---
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_10
+
+const val MIN_LENGTH = 4
+
+fun lengthValid(input: String) = input.length >= MIN_LENGTH
+
+fun main() {
+
+    println("Введите логин: ")
+    val userLogin = readln()
+    println("Введите пароль: ")
+    val userPassword = readln()
+
+    when (lengthValid(userLogin) && lengthValid(userPassword)) {
+        true -> println("Добро пожаловать!")
+        else -> println("Логин или пароль недостаточно длинные")
+    }
+
+}
