29b98854a7c104bd58090c833a9fc13e68c5a668
kamgi <kamgidayatov@gmail.com>
2026-01-02 17:29:08 +0300

    KS-11-4- Change user info

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_11
+
+class CategoryRecipe(
+    coverFood: Int,
+    nameFood: List<String>,
+    bioFood: String,
+)
+
+class Recipe(
+    coverFood: Int,
+    nameTypeFood: List<String>,
+)
+
+class Ingredient(
+    portionsCount: Int,
+    ingredientsList: List<String>,
+    countIngredientsList: List<Int>,
+    recipeManual: String
+)
