79d46191d1279180d99a78f2a31b0a8bf8eb16d5
kamgi <kamgidayatov@gmail.com>
2026-01-23 16:31:57 +0300

    KS-13-2- Using Elvis operator in printInfo() function

---
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_13
+
+class PhoneDirectory1(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String?,
+) {
+
+    fun printInfo() {
+        println(
+            "- Имя: ${this.name}\n" +
+                    "- Номер телефона: ${this.phoneNumber}\n" +
+                    "- Компания: ${company ?: "<не указано>"}\n"
+        )
+    }
+}
+
+fun main() {
+
+    val contact1 = PhoneDirectory1("Tolya", 87651658998, null)
+    contact1.printInfo()
+    val contact2 = PhoneDirectory1("Rombik", 89993275513, "Google")
+    contact2.printInfo()
+}
