84b213b037c3959b918d35aa48bfc1b3bcf51643
kamgi <kamgidayatov@gmail.com>
2026-01-23 13:30:45 +0300

    KS-12-3- Weather class Kelvin to Celsius

---
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_12
+
+const val ONE_CELSIUS_IN_KELVIN = 273.15
+
+class Weather2(
+    _dayTemprature: Int,
+    _nightTemprature: Int,
+    _rainfall: Int,
+) {
+
+    var dayTemprature = (_dayTemprature - ONE_CELSIUS_IN_KELVIN).toInt()
+    var nightTemprature = (_nightTemprature - ONE_CELSIUS_IN_KELVIN).toInt()
+    var rainfall = _rainfall
+
+    fun printInfo() {
+        println(
+            "Дневаная температура: ${this.dayTemprature}\n" +
+                    "Ночная температура: ${this.nightTemprature}\n" +
+                    "Количество осадков: ${this.rainfall}"
+        )
+    }
+
+}
+
+fun main() {
+
+    val sunday = Weather2(263, 295, 123)
+    sunday.printInfo()
+
+}
