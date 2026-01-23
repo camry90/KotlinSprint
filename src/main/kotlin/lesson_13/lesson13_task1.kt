a7a8078fad208e36e36c8e18d3a3b2997fee6f14
kamgi <kamgidayatov@gmail.com>
2026-01-23 16:19:44 +0300

    KS-13-1- Phone directory with null valuable

---
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_13
+
+class PhoneDirectory(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String?,
+)
+
+fun main() {
+
+    val info = PhoneDirectory("Ruri", 89621237190, null)
+
+}
+
