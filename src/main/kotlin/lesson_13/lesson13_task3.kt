ee125019b7783b9609e8e103a52cdf651dd15512
kamgi <kamgidayatov@gmail.com>
2026-01-25 19:02:57 +0300

    KS-13-3- fix toSet()

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -1,32 +1,22 @@
 package org.example.lesson_13
 
-class PhoneDirectory2(
+class PhoneBookEntry2(
     val name: String,
     val phoneNumber: Long,
     val company: String?,
-) {
-
-    fun printInfo() {
-        println(
-            "- Имя: ${name}\n" +
-                    "- Номер телефона: ${phoneNumber}\n" +
-                    "- Компания: ${company ?: "<не указано>"}\n"
-        )
-    }
-}
+)
 
 fun main() {
 
-    val contactList = mutableListOf(
-        PhoneDirectory2("Kyle", 89761231265, null),
-        PhoneDirectory2("Lisa", 86714329871, "Yahoo"),
-        PhoneDirectory2("Takaba", 89916723432, null),
-        PhoneDirectory2("Ian", 89767776109, "null"),
-        PhoneDirectory2("Mira", 89767776109, "Yandex"),
+    val contactList = listOf(
+        PhoneBookEntry2("Kyle", 89761231265, null),
+        PhoneBookEntry2("Lisa", 86714329871, "Yahoo"),
+        PhoneBookEntry2("Takaba", 89916723432, null),
+        PhoneBookEntry2("Ian", 89767776109, "null"),
+        PhoneBookEntry2("Mira", 89767776109, "Yahoo"),
     )
 
-    val companies = contactList.mapNotNull { it.company }
-    companies.toSet()
+    val companies = contactList.mapNotNull { it.company }.toSet()
     println(companies)
 
 }
