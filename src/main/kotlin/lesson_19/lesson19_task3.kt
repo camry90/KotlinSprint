dc2c84b2f0ddd43c72a9ce0830177f635eb86984
kamgi <kamgidayatov@gmail.com>
2026-02-05 17:18:40 +0300

    KS-19-3- Using TODO

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_19
+
+class CosmicShip {
+
+    fun fly() {
+        //TODO нужна дополнительная логика
+    }
+
+    fun land() {
+        TODO()
+    }
+
+    fun shoot() {
+
+    }
+
+}
+
+fun main() {
+
+    val ship1 = CosmicShip()
+    ship1.land()
+}
+
