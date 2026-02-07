44e7b218498731bdf3500f8cc274abc74f1f59e8
kamgi <kamgidayatov@gmail.com>
2026-02-07 15:59:07 +0300

    KS-21-1- Vowel count

---
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -0,0 +1,20 @@
+﻿package org.example.lesson_21
+
+fun main() {
+
+    val hello = "ASDAJJq1ooooo"
+    println(hello.vowelCount())
+
+}
+
+fun String.vowelCount(): Int {
+
+    var counter = 0
+    for (ch in this) {
+        if (ch in "AOEIUaoeiu") {
+            counter++
+        }
+    }
+    return counter
+
+}
