e736a4ee3e6144bb71abf3ceab24d397ad0ca4c3
kamgi <kamgidayatov@gmail.com>
2026-02-08 19:03:52 +0300

    KS-22-4- Using copy , pattern MVVM

---
+++ src/main/kotlin/lesson_22/lesson22_task4.kt
@@ -0,0 +1,15 @@
+﻿package org.example.lesson_22
+
+class MainScreenViewModel(var mainScreenState: MainScreenState) {
+
+    fun loadData() {
+        mainScreenState = mainScreenState.copy(data = "Нет данных")
+        mainScreenState = mainScreenState.copy(isLoading = true)
+        mainScreenState = mainScreenState.copy(isLoading = false)
+    }
+}
+
+data class MainScreenState(
+    val data: String,
+    val isLoading: Boolean = false,
+)
