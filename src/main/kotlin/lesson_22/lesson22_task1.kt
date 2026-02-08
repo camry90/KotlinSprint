2b42ba4db011083656aa568c7833b04127528dd3
kamgi <kamgidayatov@gmail.com>
2026-02-08 17:56:44 +0300

    KS-22-1- fix

--- src/main/kotlin/lesson_22/lesson22_task1.kt
+++ src/main/kotlin/lesson_22/lesson22_task1.kt
@@ -1,28 +1,21 @@
 ﻿package org.example.lesson_22
 
-class ReguralBook(val title: String, val author: String) {
+class RegularBook(val title: String, val author: String)
 
-}
-
-data class DataBook(val title: String, val author: String) {
-
-}
+data class DataBook(val title: String, val author: String)
 
 fun main() {
-
 
     val book1 = DataBook("Michael Jackson", "Michael Jackson")
     val book2 = DataBook("Michael Jackson", "Michael Jackson")
 
-    val book3 = ReguralBook("Chernobyl", "Sasha Beliy")
-    val book4 = ReguralBook("Chernobyl", "Sasha Beliy")
+    val book3 = RegularBook("Chernobyl", "Sasha Beliy")
+    val book4 = RegularBook("Chernobyl", "Sasha Beliy")
 
     // дата класс сравнивает данные
     println(book1 == book2)
 
     // обычный класс ссылки
     println(book3 == book4)
-
-
 
 }
