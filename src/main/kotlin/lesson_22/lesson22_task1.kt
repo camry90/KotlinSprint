6bcc575577483562d95775264825d8ef83dde2af
kamgi <kamgidayatov@gmail.com>
2026-02-08 17:54:49 +0300

    KS-22-1- Data class vs Class in equals

---
+++ src/main/kotlin/lesson_22/lesson22_task1.kt
@@ -0,0 +1,28 @@
+﻿package org.example.lesson_22
+
+class ReguralBook(val title: String, val author: String) {
+
+}
+
+data class DataBook(val title: String, val author: String) {
+
+}
+
+fun main() {
+
+
+    val book1 = DataBook("Michael Jackson", "Michael Jackson")
+    val book2 = DataBook("Michael Jackson", "Michael Jackson")
+
+    val book3 = ReguralBook("Chernobyl", "Sasha Beliy")
+    val book4 = ReguralBook("Chernobyl", "Sasha Beliy")
+
+    // дата класс сравнивает данные
+    println(book1 == book2)
+
+    // обычный класс ссылки
+    println(book3 == book4)
+
+
+
+}
