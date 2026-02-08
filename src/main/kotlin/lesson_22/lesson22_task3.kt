68a49fe89f9439d77d9a55bd0887663421c7b339
kamgi <kamgidayatov@gmail.com>
2026-02-08 18:13:31 +0300

    KS-22-3- Destructuring declarations

---
+++ src/main/kotlin/lesson_22/lesson22_task3.kt
@@ -0,0 +1,12 @@
+﻿package org.example.lesson_22
+
+data class Person223(
+    var name: String,
+    var age: Int,
+    var height: Int
+)
+
+fun main() {
+    val (name, age, height) = Person223("Kamran", 20, 181)
+    println("$name, $age, $height")
+}
