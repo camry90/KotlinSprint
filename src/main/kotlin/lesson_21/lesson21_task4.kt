e1bd1e82d1bce9a07d6b7cc09705c6e3a64a11af
kamgi <kamgidayatov@gmail.com>
2026-02-11 19:44:53 +0300

    KS-21-4- Write lowercase text in start file

---
+++ src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -0,0 +1,19 @@
+﻿package org.example.lesson_21
+
+import java.io.File
+
+fun main() {
+
+    val file = File("lesson21-4.txt")
+    file.writeInStartFile("Start")
+
+}
+
+fun File.writeInStartFile(string: String) {
+
+    val stringList: MutableList<String> = this.readLines().toMutableList()
+    println(stringList)
+    val lowerString = string.lowercase()
+    stringList.addFirst(lowerString)
+    writeText(stringList.joinToString(separator = " "))
+}
