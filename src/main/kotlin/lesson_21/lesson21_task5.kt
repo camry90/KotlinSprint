c6e016729e2c7d5745d14687d8626e303d086c96
kamgi <kamgidayatov@gmail.com>
2026-02-11 21:56:23 +0300

    KS-21-5- Print max skill xp

---
+++ src/main/kotlin/lesson_21/lesson21_task5.kt
@@ -0,0 +1,21 @@
+﻿package org.example.lesson_21
+
+class Player215(val skills: MutableMap<String, Int>) {
+
+    fun gainExperience(skill: String, xp: Int) {
+        skills[skill] = skills.getOrDefault(skill, 0) + xp
+    }
+}
+
+fun main() {
+
+    val player = Player215(mutableMapOf())
+    player.gainExperience("Fireball", 80)
+    player.gainExperience("Water splash", 100)
+    player.gainExperience("Doom", 100)
+    println(player.skills.maxCategory())
+}
+
+fun Map<String, Int>.maxCategory(): String {
+    return this.maxBy { it.key }.key
+}
