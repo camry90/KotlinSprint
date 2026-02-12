e6d909b5b087debfa5720c31fe0c9989dea9b4da
kamgi <kamgidayatov@gmail.com>
2026-02-12 15:21:52 +0300

    KS-22-5- Galactic guide

---
+++ src/main/kotlin/lesson_22/lesson22_task5.kt
@@ -0,0 +1,22 @@
+﻿package org.example.lesson_22
+
+data class GalacticGuide(
+    val place: String,
+    val placeBio: String,
+    val data: String,
+    val placeDistance: String,
+)
+
+fun main() {
+
+    val starSystem = GalacticGuide(
+        "Alpha Centauri",
+        "Звёздная система, расположенная в созвездии Центавра",
+        "26700 году",
+        "4.3 св. лет"
+    )
+
+    val (place, placeBio, data, placeDistance) = starSystem
+
+    println("$place, $placeBio, $data, $placeDistance")
+}
